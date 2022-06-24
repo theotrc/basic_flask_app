d3ff7bc71343c41113c2bcede3e9bdc21a06c0ed
theo <theotricot12@gmail.com>
2022-06-24 13:25:46 +0200

    try to resolve conflict with dotenv in production

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=True)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-SECRET_KEY = os.environ.get('secret')
+SECRET_KEY = os.environ.get('SECRET')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
